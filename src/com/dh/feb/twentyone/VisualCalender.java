78b6062b8ec9d29101848882dabd3d77fa1f6a61
admin <605489042@qq.com>
2017-02-25 00:34:23 +0800

    fix day of week

--- src/com/dh/feb/twentyone/VisualCalender.java
+++ src/com/dh/feb/twentyone/VisualCalender.java
@@ -16,21 +16,23 @@ public class VisualCalender {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//	
-//		String time = scanner.nextLine().toString();
-		String  time = "2017-2-23";
+		Scanner scanner = new Scanner(System.in);
+	
+		String time = scanner.nextLine().toString();
+		//String  time = "2017-2-23";
 		DateFormat nowdate = new SimpleDateFormat("yyyy-MM-dd");
 		//日期格式
 		Calendar calendar = new GregorianCalendar();
-		int day = calendar.get(Calendar.DATE);
-		//设置格式j
+		
+		//设置格式
 		try {
 			Date date = nowdate.parse(time);
 			
 			//字符串转换为日期
 			calendar.setTime(date);
-			//将日期设置到calendar对象
+			int day = calendar.get(Calendar.DATE);
+			//System.out.println("day="+day+"  Calendar.DATE="+Calendar.DATE);
+			//将日期设置到calendar对象  
 			calendar.set(calendar.DAY_OF_MONTH, 1);
 
 			//System.out.println(calendar.get(calendar.DAY_OF_WEEK));
@@ -40,12 +42,12 @@ public class VisualCalender {
 			System.out.println(calendar.getActualMaximum(calendar.DATE));
 			System.out.println("一\t二\t三\t四\t五\t六\t日");
 			for(int j=1;j<calendar.get(calendar.DAY_OF_WEEK)-1;j++){
-				System.out.print("*\t");
+				System.out.print("\t");
 			}
 			//System.out.println(calendar.get(calendar.SATURDAY));
 			for(int i=1;i<=calendar.getActualMaximum(calendar.DATE);i++){
 				if(i==day){
-					System.out.print("@");
+					System.out.print("+");
 				}
 				System.out.print(i+"\t");
 				int nowday = calendar.get(calendar.DAY_OF_WEEK);
