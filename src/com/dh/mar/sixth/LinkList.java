dc69a38c8b255a790521e9837277f88f32723480
admin <605489042@qq.com>
2017-03-07 23:21:15 +0800

    使用jdk给定方法实现linkedlist中add方法进入无限死循环？还是双向链表？还有其他解法？

--- src/com/dh/mar/sixth/LinkList.java
+++ src/com/dh/mar/sixth/LinkList.java
@@ -2,11 +2,23 @@ package com.dh.mar.sixth;
 
 import java.util.Date;
 
+
+
 public class LinkList {
-	private static final Object Object = null;
-	private Node first;
-	private Node last;
+	Node first;
+	Node last;
 	private int size;
+	public void add(Object obj) {
+	final Node l = last;
+    final Node newNode = new Node(last,obj, null);
+	    //调用node带参数构造函数
+	    last = newNode;
+	    if (l == null)
+	        first = newNode;
+	    else
+	    	l.next = newNode;
+	    size++;
+	}
 	public static void main(String[] args) {
 		LinkList list = new LinkList();
 		list.add("aaa");
@@ -15,7 +27,7 @@ public class LinkList {
 		list.add(new Date());
 		list.add(new Node());
 		System.out.println(list.size);
-		//System.out.println(list.get(1));
+		System.out.println(list.get(10));
 	}
 	private void rangecheck(int index){
 		if(index<0||index>size){
@@ -39,24 +51,7 @@ public class LinkList {
 				temp = temp.next;
 			}
 		}
-		return temp.getObj();
-	}
-	private void add(Object obj) {
-		Node n = new Node();
-		if(first==null){
-			//如果第一个为空，那么久没元素，第一个和最后一个重合
-			n.previous=null;
-			n.setObj(obj);
-			n.next=null;
-			first = n;
-			last = n;
-		}else{
-			n.previous=last;
-			n.setObj(obj);
-			n.next=null;
-			last = n;
-		}
-		size++;
+		return temp.obj;
 	}
 	public int size(){
 		return size;
--- src/com/dh/mar/sixth/Node.java
+++ src/com/dh/mar/sixth/Node.java
@@ -2,15 +2,8 @@ package com.dh.mar.sixth;
 
 public class Node {
 	Node previous; //上一个节点，节点类型
-	private Object obj;
+	Object obj;
 	Node next;   //下一个节点，节点类型
-	public Object getObj() {
-		return obj;
-	}
-	public void setObj(Object obj) {
-		this.obj = obj;
-	}
-
 	public Node(){
 		
 	}
